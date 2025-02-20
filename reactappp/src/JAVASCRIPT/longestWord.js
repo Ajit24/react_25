64358b555bb7d7762785290a0f3c929787fcd9f0
Ajit Kumar Pandey <87422047+Ajit24@users.noreply.github.com>
2025-02-20 12:13:02 +0530

    longest word in string

---
+++ reactappp/src/JAVASCRIPT/longestWord.js
@@ -0,0 +1,20 @@
+const findLongestWord = (str) => {
+    // if (str.trim().length === 0) {
+    //   return false;
+    // }
+  
+    let words = str.split(" ");
+    words = words.sort((a, b) => b.length - a.length);
+    console.log(words);
+    // return words.at(-1);
+    return words[0];
+  };
+  
+  console.log(
+    findLongestWord(
+      "Watch Thapa Technical javascript awesomethapatechnicalllll course on youtube"
+    )
+  );
+
+
+  // 
